331ac6378044daa0a11204d4f2208c93ab467ddd
Shengbo Jia <shengbojia@gmail.com>
2019-07-20 19:00:07 -0400

    added new class for variable environment

---
+++ src/main/java/com/shengbojia/lox/Environment.java
@@ -0,0 +1,39 @@
+package com.shengbojia.lox;
+
+import com.shengbojia.lox.errors.RuntimeError;
+import com.shengbojia.lox.token.Token;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A wrapper of a hash map which stores variables as identifier-value pairs.
+ */
+public class Environment {
+    private final Map<String, Object> values = new HashMap<>();
+
+    /**
+     * Adds a identifier-value pair to the environment. Allows for redefining of a global variable.
+     *
+     * @param name the name of the variable
+     * @param value the value of the variable
+     */
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    /**
+     * Looks up a variable from the environment based on the inputted token. Throws a runtime error if the variable
+     * if accessed before being defined.
+     *
+     * @param name the identifier token of the desired variable
+     * @return the value of the referenced variable
+     */
+    Object get(Token name) {
+        if (values.containsKey(name.lexeme)) {
+            return values.get(name.lexeme);
+        }
+
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+}
