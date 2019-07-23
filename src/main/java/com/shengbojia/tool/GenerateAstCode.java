c7dbd676113e3786776f3c754b143b9ad85c8774
Shengbo Jia <shengbojia@gmail.com>
2019-07-22 20:11:18 -0400

    added code to generate if stmt

--- src/main/java/com/shengbojia/tool/GenerateAstCode.java
+++ src/main/java/com/shengbojia/tool/GenerateAstCode.java
@@ -35,6 +35,7 @@ public class GenerateAstCode {
         defineAst(outputDir, packageName, "Stmt", Arrays.asList(
                 "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
+                "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print      : Expr expression",
                 "Var        : Token name, Expr initializer"
         ));
